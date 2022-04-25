941ea04bd55be4746119bb8958ca6342a767867a
aiadventures <aiadventures.pune@gmail.com>
2022-04-25 11:42:39 +0530

    Create Q1.py

---
+++ Q1.py
@@ -0,0 +1,14 @@
+# Question 1
+
+""" Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order."""
+
+  def twoSum(nums: List[int], target: int) -> List[int]:
+  
+  
+  # Complete the funciton definition so that it should return a list containing the indices of the numbers whose sum is equal to the target.
+  
+  pass
